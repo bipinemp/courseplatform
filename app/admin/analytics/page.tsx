e6d2973463f8d2f263a309bd5ebf92242bb1123a
bipinemp <bhandaribipin3.39@gmail.com>
2024-03-21 21:11:07 +0545

    finally completed dynamic layout for user and admin

--- app/admin/analytics/page.tsx
+++ app/admin/analytics/page.tsx
@@ -1,10 +1,13 @@
+import Container from "@/components/Container";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="pl-52 pt-32">
-      <h1>Admin Analytics</h1>
-    </div>
+    <Container>
+      <div className="pl-52 pt-32">
+        <h1>Admin Analytics</h1>
+      </div>
+    </Container>
   );
 };
 
---
+++ components/admin/AdminMode.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { LogOut } from "lucide-react";
+
+const AdminMode = () => {
+  const pathname = usePathname();
+  const isAdminRoute = pathname?.startsWith("/admin");
+
+  return (
+    <>
+      {isAdminRoute ? (
+        <Link href={"/"}>
+          <Button
+            className="flex items-center gap-2 font-semibold"
+            variant={"destructive"}
+          >
+            <LogOut className="h-4 w-4" strokeWidth={3} /> Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href={"/admin/courses"}>
+          <Button className="font-semibold">Admin Mode</Button>
+        </Link>
+      )}
+    </>
+  );
+};
+
+export default AdminMode;
