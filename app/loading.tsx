2a4234008cdcc90d20197d9eca6485c27252816e
bipinemp <bhandaribipin3.39@gmail.com>
2024-04-03 18:45:53 +0545

    changes

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import GlobalLoading from "@/components/GlobalLoading";
-import React from "react";
-
-const loading = () => {
-  return <GlobalLoading alreadyMargin={true} />;
-};
-
-export default loading;
