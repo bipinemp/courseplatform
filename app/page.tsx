c331722cddb923ad98adf2ef28cc8c14e69b244d
bipinemp <bhandaribipin3.39@gmail.com>
2024-05-02 18:20:48 +0545

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/Container";
 import Browse from "@/components/user/Browse";
 
 export default function Home() {
-  throw new Error("error message :fkdljfdl");
+  
   return (
     <Container>
       <Browse isDashboard={false} />
